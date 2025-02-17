cc82d4aee63928ed3755b1e9e3c460d819161d21
Taiyo <96026204+tio0o0o0o@users.noreply.github.com>
2025-02-18 06:47:19 +1300

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(ttf|otf|woff|woff2)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
