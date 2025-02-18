915c614f7ca44ea62859c475aa89c3045baa962b
Taiyo <taiyoperson@gmail.com>
2025-02-18 16:52:18 +1300

    chore: Config ESLint for browser

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,6 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
